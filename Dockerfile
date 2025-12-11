4147da913908606bbc8cd55f358fba0fe55b8859
Musab Bozkurt <mbozkurt@migros.com.tr>
2025-12-11 19:05:50 +0300

    upgrade-spring-boot-to-4-and-java-to-25

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:21-jdk-slim
+FROM openjdk:25-rc-slim
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} kafka-debezium-service.jar
 ENTRYPOINT ["java","-jar","/kafka-debezium-service.jar"]
--- src/test/java/com/mb/kafkadebeziumservice/api/controller/ConsumerControllerTest.java
+++ src/test/java/com/mb/kafkadebeziumservice/api/controller/ConsumerControllerTest.java
@@ -3,7 +3,7 @@ package com.mb.kafkadebeziumservice.api.controller;
 import com.mb.kafkadebeziumservice.queue.consumer.ConsumerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
